38cfd98c1ddae98b9c5392bbaeff06c478f7c125
Christo Martin <christomartinog@gmail.com>
2026-02-28 21:02:59 +0530

    Fixed MySQL VARCHAR length issue

--- backend/src/models.py
+++ backend/src/models.py
@@ -2,26 +2,32 @@ from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from .database import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+
+    full_name = Column(String(255), index=True)
+    email = Column(String(255), unique=True, index=True)
+    hashed_password = Column(String(255))
     is_active = Column(Boolean, default=True)
 
     fields = relationship("Field", back_populates="owner")
+
 
 class Field(Base):
     __tablename__ = "fields"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    location = Column(String)
+
+    name = Column(String(255), index=True)
+    location = Column(String(255))
     size = Column(Float)
-    crop_type = Column(String, nullable=True) # "None" or crop name
-    soil_type = Column(String, nullable=True)
+
+    crop_type = Column(String(100), nullable=True)  # crop name
+    soil_type = Column(String(100), nullable=True)
+
     owner_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="fields")
